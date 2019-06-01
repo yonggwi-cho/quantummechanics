d34bf6d5570477e9e245b21e259421c277084f52
Yong-Gwi Cho <cho@chou-no-iMac.local>
2019-06-01 14:42:51 +0900

    added sources.

---
+++ schrodinger_1d.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import argparse
+import numpy as np
+
+class equation :
+    def __init__(self,n):
+        n_mesh = 2*n
+        psi = np.array(n,type=float)
+        
+    # solve time-independent schrodinger eq.
+    def solve(self,):
+        
+
+if __name__ == "__main__" :
+    schro = equation()
+    scshro.solve()
+
